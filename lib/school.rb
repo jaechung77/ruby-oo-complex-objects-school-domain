a6ad766c72b17a9a177b28bb923ba347ee96465d
jaechung77 <jaechung77@gmail.com>
2021-08-30 00:13:47 +1000

    lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+    attr_accessor :student, :grade
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @school = Hash.new {|h,k| h[k] = [] }
+    end 
+    def roster   
+        @school    
+    end    
+    #{ :grade => [student] }
+    def add_student(student, grade)    
+        @school[grade] << student    
+    end 
+    def grade(grade) 
+        @school[grade]    
+    end   
+    def sort
+        result = Hash.new {|h,k| h[k] = [] }
+        @school.sort.each do |k, v|
+            result[k] = v.sort
+        end  
+        result                 
+    end      
+end    
+
